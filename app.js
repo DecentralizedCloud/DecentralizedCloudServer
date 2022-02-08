d665390c5eaad1a44a8adc9eb3d8044c7070dc49
yash10102002 <yashkalavadiya1010@gmail.com>
2022-02-08 14:18:44 +0530

    few changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 3000;
-
+console.log("HEllo")
 app.listen(port, () => {
   console.log(`Up and running on :${port}!!`);
 });
