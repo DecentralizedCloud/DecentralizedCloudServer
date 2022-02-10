6911bd155564bf90a83e9f9f49cb6e565c1142c3
yash10102002 <yashkalavadiya1010@gmail.com>
2022-02-10 18:45:43 +0530

    almost done with upload

--- controllers/uploadFile.js
+++ controllers/uploadFile.js
@@ -1,15 +1,29 @@
 const fs = require("fs");
-
+const admin = require("../admin");
 const ipfs = require("../ipfsClient");
 
 exports.uploadFile = async (req, res) => {
   //   console.log(req);
   const { fields, files } = req;
   console.log(fields);
-  console.log(files);
+  console.log(files.file);
   const hash = await addFile(fields.fileName, files.file.filepath);
-  console.log(hash);
-  res.status(200).json({ hash });
+  console.log(hash.toString());
+  const { userId, apiKey, projectId, projectName, fileName, reference } =
+    fields;
+  const dbRefPath = `${userId}/projects/${projectId}/data/${reference}`;
+  const dbRef = admin.database().ref(dbRefPath);
+  const extension = files.file.originalFilename.split(".")[1];
+  let obj = {
+    createdOn: admin.database.ServerValue.TIMESTAMP,
+    downloadURL: `https://ipfs.io/ipfs/${hash}`,
+    extension,
+    fileName: fileName.split(".")[0],
+    hash: hash.toString(),
+  };
+  console.log(obj);
+  await dbRef.child(fileName).set(obj);
+  res.status(200).json({ obj });
 };
 
 //Helpers
