92c88c14c5cb098c8c743a783394f3ed9633792c
yash10102002 <yashkalavadiya1010@gmail.com>
2022-02-13 17:34:11 +0530

    almost done with GET file

--- routes/getRoutes.js
+++ routes/getRoutes.js
@@ -1,9 +1,31 @@
 const express = require("express");
-
+const admin = require("../admin");
+var ipfsAPI = require("../ipfsAPI");
 const router = express.Router();
 
-router.post("/getFile", (req, res) => {
-  res.send("dummy file sent");
+/*
+ * Request to retrive file from IPFS
+ */
+router.get("/getFile", async (req, res) => {
+  console.log(req.body);
+  const { userId, apiKey, projectId, projectName, filePath } = req.body;
+
+  const dbRef = admin
+    .database()
+    .ref(`${userId}/projects/${projectId}/data/${filePath}`);
+
+  let data = await (await dbRef.get()).val();
+
+  console.log(data);
+
+  const { hash, mimetype } = data;
+  ipfsAPI.cat(hash, (err, data) => {
+    if (err || !data)
+      return res.status(400).json({ error: "Some error occured" });
+
+    res.set("Content-Type", mimetype);
+    res.send(data);
+  });
 });
 
 module.exports = router;
