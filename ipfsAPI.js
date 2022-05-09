b0ca39c58313822af32bd28a790a902c7a91d27d
GauravChawda <grchawda16@gmail.com>
2022-05-09 18:07:55 +0530

    ip added

--- ipfsAPI.js
+++ ipfsAPI.js
@@ -1,5 +1,5 @@
 const ipfs = require("ipfs-api");
 
-var ipfsAPI = ipfs("127.0.0.1", "5001", { protocol: "http" }); // leaving out the arguments will default to these values
+var ipfsAPI = ipfs("20.213.239.201", "5001", { protocol: "http" }); // leaving out the arguments will default to these values
 
 module.exports = ipfsAPI;
--- ipfsClient.js
+++ ipfsClient.js
@@ -1,9 +1,9 @@
 const ipfs = require("ipfs-http-client");
 
 const ipfsClient = ipfs.create({
-  host: "127.0.0.1",
-  port: "5001",
-  protocol: "http",
+	host: "20.213.239.201",
+	port: "5001",
+	protocol: "http",
 });
 
 module.exports = ipfsClient;
